3b51df5dc1d14bdc4201a6f8ce056398a88c45f1
Thameera1002 <thameera1002@gmail.com>
2022-09-12 20:07:19 +0530

    Home form UI designed.

--- src/lk/cvgen/controller/HomeFormController.java
+++ src/lk/cvgen/controller/HomeFormController.java
@@ -1,4 +1,21 @@
 package lk.cvgen.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+
 public class HomeFormController {
+    public void btnGenSLTemplateOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void btnGenUSATemplateOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void btnGenUKTemplateOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void btnGenUAETemplateOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void btnGenOtherTemplateOnAction(ActionEvent actionEvent) {
+    }
 }
